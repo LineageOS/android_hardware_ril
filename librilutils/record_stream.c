8ff6da80ea8d21b774878250ff741c11d78ab9ae
MethodWhite <methodwhite@proton.me>
2026-05-07 17:13:39 -0400

    hardware/ril: Fix unchecked malloc/calloc null dereferences
    * Fix record_stream.c calloc/malloc null deref (CRITICAL crash rild)
    * Fix atchannel.c addIntermediate() unchecked malloc and strdup
    
    Change-Id: I20fb6bf5b18046a3c7ec73065d834d10c96d445a

--- librilutils/record_stream.c
+++ librilutils/record_stream.c
@@ -49,10 +49,17 @@ extern RecordStream *record_stream_new(int fd, size_t maxRecordLen)
     assert (maxRecordLen <= 0xffff);
 
     ret = (RecordStream *)calloc(1, sizeof(RecordStream));
+    if (ret == NULL) {
+        return NULL;
+    }
 
     ret->fd = fd;
     ret->maxRecordLen = maxRecordLen;
     ret->buffer = (unsigned char *)malloc (maxRecordLen + HEADER_SIZE);
+    if (ret->buffer == NULL) {
+        free(ret);
+        return NULL;
+    }
 
     ret->unconsumed = ret->buffer;
     ret->read_end = ret->buffer;
